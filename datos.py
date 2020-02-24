42ceb421005ec975497e5884911e281921b3d031
manuelpintado <if707405@iteso.mx>
2020-02-24 08:50:22 -0600

    Archivos inicializados para llenar

---
+++ datos.py
@@ -0,0 +1,7 @@
+# -- ------------------------------------------------------------------------------------ -- #
+# -- proyecto: Microestructura y Sistemas de Trading - Laboratorio 2 - Behavioral Finance
+# -- archivo: datos.py - datos generales para uso en el proyecto
+# -- mantiene: IF Manuel Pintado
+# -- repositorio: https://github.com/manuelpintado/LAB_2_MPD.git
+# -- ------------------------------------------------------------------------------------ -- #
+
---
+++ main.py
@@ -0,0 +1,7 @@
+# -- ------------------------------------------------------------------------------------ -- #
+# -- proyecto: Microestructura y Sistemas de Trading - Laboratorio 2 - Behavioral Finance
+# -- archivo: main.py - codigo principal del proyecto
+# -- mantiene: IF Manuel Pintado
+# -- repositorio: https://github.com/manuelpintado/LAB_2_MPD.git
+# -- ------------------------------------------------------------------------------------ -- #
+
